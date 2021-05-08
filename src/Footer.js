d64eb854eb052f71a41f10b5712bfb9a757a1cf5
yogx <yogx4u@gmail.com>
2021-05-08 23:03:17 +0530

    First

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="App-footer">
+            <div style={{position: "absolute", bottom: "10px", right: "10px"}}>&copy; Developed using React by YOGESH PATIL</div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="App-header">
+        <h1>YOGESH PATIL</h1>
+        <p>Full Stack Developer</p>
+      </header>
+    )
+}
+
+export default Header;
